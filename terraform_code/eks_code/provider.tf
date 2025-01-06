e52d6d563e17811d964a36142dfab6429913d1b5
shettyvijay32 <144682707+shettyvijay32@users.noreply.github.com>
2025-01-06 17:03:18 +0530

    Update provider.tf

--- terraform_code/eks_code/provider.tf
+++ terraform_code/eks_code/provider.tf
@@ -1,8 +1,8 @@
 locals {
-  region = "us-east-1"
+  region = "ap-south-1"
   name   = "amazon-prime-cluster"
   vpc_cidr = "10.0.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
+  azs      = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
@@ -12,5 +12,5 @@ locals {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
